c1baed071fc3241acdd0bb6641524141754a0ac1
YujiSoftware <yuji.software+github@gmail.com>
2016-11-28 02:41:53 +0900

    SlideTwitter に loggerを追加

--- SlideTwitter/src/main/java/tech/slideshare/twitter/Main.java
+++ SlideTwitter/src/main/java/tech/slideshare/twitter/Main.java
@@ -1,5 +1,7 @@
 package tech.slideshare.twitter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import tech.slideshare.twitter.database.SlideDao;
 import tech.slideshare.twitter.database.SlideDto;
 import twitter4j.Status;
@@ -12,21 +14,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String... args) throws SQLException, TwitterException {
         String user = args[0];
         String password = args[1];
+
+        logger.info("Start {}", Main.class.toString());
 
         try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tech_slideshare", user, password)) {
             con.setAutoCommit(false);
 
             SlideDto dto = new SlideDao(con).dequeue();
-
             Twitter twitter = TwitterFactory.getSingleton();
             Status status = twitter.updateStatus(dto.getTitle() + "\r\n" + dto.getUrl());
 
-            System.out.println("Successfully updated the status to [" + status.getText() + "].");
+            logger.info("Successfully updated the status to [{}].", status.getText());
 
             con.commit();
         }
+
+        logger.info("End {}", Main.class.toString());
     }
 }
