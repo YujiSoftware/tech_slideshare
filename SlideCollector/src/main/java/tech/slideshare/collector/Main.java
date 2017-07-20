99ceecb1372d2dbb6fb1416c802845c0eaaccd01
YujiSoftware <yuji.software+github@gmail.com>
2017-07-21 01:54:13 +0900

    speakerdeck の Embedded ページが含まれていると収集中にエラーになる問題を修正

--- SlideCollector/src/main/java/tech/slideshare/collector/Main.java
+++ SlideCollector/src/main/java/tech/slideshare/collector/Main.java
@@ -74,6 +74,7 @@ public class Main {
                 r.items.stream()
                         .filter(i -> i.subject != null && i.subject.equals("テクノロジー"))
                         .filter(i -> !i.link.contains("://www.slideshare.net/slideshow/embed_code/"))
+                        .filter(i -> !i.link.contains("://speakerdeck.com/player/"))
                         .forEach(item -> {
                             try {
                                 String title = item.title.replace(" // Speaker Deck", "");
