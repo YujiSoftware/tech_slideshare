56c7cae1579825a14396c6e5a01cccd633b30d8e
YujiSoftware <yuji.software+github@gmail.com>
2021-09-18 18:14:15 +0900

    Backpaper0 のURL正規化に対応

--- SlideCollector/src/main/java/tech/slideshare/collector/Backpaper0Collector.java
+++ SlideCollector/src/main/java/tech/slideshare/collector/Backpaper0Collector.java
@@ -1,21 +1,46 @@
 package tech.slideshare.collector;
 
 import jakarta.xml.bind.JAXBException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.slideshare.rss.Bookmark;
 import tech.slideshare.rss.HatenaBookmark;
+import tech.slideshare.rss.Item;
 
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 public class Backpaper0Collector implements SlideCollector {
+    private static final Logger logger = LoggerFactory.getLogger(GoogleSlideCollector.class);
 
-    private final HatenaBookmark collector
-            = new HatenaBookmark("https://b.hatena.ne.jp/entrylist?url=http%3A%2F%2Fbackpaper0.github.io%2Fghosts%2F&mode=rss");
+    private final Bookmark bookmark;
+
+    public Backpaper0Collector() {
+        this.bookmark = new HatenaBookmark("https://b.hatena.ne.jp/entrylist?url=http%3A%2F%2Fbackpaper0.github.io%2Fghosts%2F&mode=rss");
+    }
+
+    public Backpaper0Collector(Bookmark bookmark) {
+        this.bookmark = bookmark;
+    }
 
     @Override
     public Stream<Slide> collect() throws JAXBException, MalformedURLException {
-        return collector.get()
-                .peek(i -> i.link = i.link.replaceAll("#.*$", ""))
-                .map(i -> new Slide(i, Optional.of("うらがみ, @\u200Bbackpaper0")));
+        return bookmark.get()
+                .map(i -> getSlide(i).orElse(null))
+                .filter(Objects::nonNull);
+    }
+
+    private static Optional<Slide> getSlide(Item item) {
+        try {
+            URL url = new URL(item.link);
+            String link = new URL(url.getProtocol(), url.getHost(), url.getPath().replaceFirst("index.html", "")).toString();
+
+            return Optional.of(new Slide(item.title, link, item.date, Optional.of("うらがみ, @\u200Bbackpaper0")));
+        } catch (MalformedURLException e) {
+            return Optional.empty();
+        }
     }
 }
---
+++ SlideCollector/src/test/java/tech/slideshare/collector/Backpaper0CollectorTest.java
@@ -0,0 +1,65 @@
+package tech.slideshare.collector;
+
+import jakarta.xml.bind.JAXBException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import tech.slideshare.rss.Item;
+
+import java.net.MalformedURLException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Backpaper0CollectorTest {
+
+    private Item getItem() {
+        var item = new Item();
+        item.title = "Spring WebFluxの話";
+        item.link = "https://backpaper0.github.io/ghosts/reactive/";
+        item.description = "";
+        item.date = "2021-08-14T15:12:29Z";
+        item.subject = "テクノロジー";    // TODO: 複数あるみたい
+
+        return item;
+    }
+
+    @Test
+    public void collect() throws MalformedURLException, JAXBException {
+        var item = getItem();
+        var collector = new Backpaper0Collector(() -> Stream.of(item));
+
+        List<Slide> slides = collector.collect().collect(Collectors.toList());
+        assertEquals(1, slides.size());
+
+        Slide slide = slides.get(0);
+        assertEquals(item.title, slide.getTitle());
+        assertEquals(item.link, slide.getLink());
+        assertEquals(Optional.of("うらがみ, @\u200Bbackpaper0"), slide.getAuthor());
+        assertEquals(ZonedDateTime.of(2021, 8, 15, 0, 12, 29, 0, ZoneId.of("Asia/Tokyo")), slide.getDate());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "https://backpaper0.github.io/ghosts/reactive/#1",
+            "https://backpaper0.github.io/ghosts/reactive/index.html",
+            "https://backpaper0.github.io/ghosts/reactive/index.html#1"
+    })
+    void 正規化(String link) throws MalformedURLException, JAXBException {
+        var item = getItem();
+        item.link = link;
+        var collector = new Backpaper0Collector(() -> Stream.of(item));
+
+        List<Slide> slides = collector.collect().collect(Collectors.toList());
+        assertEquals(1, slides.size());
+
+        Slide slide = slides.get(0);
+        assertEquals("https://backpaper0.github.io/ghosts/reactive/", slide.getLink());
+
+    }
+}
