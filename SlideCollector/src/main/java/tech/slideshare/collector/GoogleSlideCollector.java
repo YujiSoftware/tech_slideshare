fbc278e81a8167c4f6ed1d4ae45437c8f2616a3a
YujiSoftware <yuji.software+github@gmail.com>
2021-09-18 18:01:45 +0900

    Google Slide のURL正規化に対応

--- SlideCollector/src/main/java/tech/slideshare/collector/GoogleSlideCollector.java
+++ SlideCollector/src/main/java/tech/slideshare/collector/GoogleSlideCollector.java
@@ -1,27 +1,93 @@
 package tech.slideshare.collector;
 
 import jakarta.xml.bind.JAXBException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.slideshare.rss.Bookmark;
 import tech.slideshare.rss.HatenaBookmark;
+import tech.slideshare.rss.Item;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 public class GoogleSlideCollector implements SlideCollector {
 
-    private final HatenaBookmark collector
-            = new HatenaBookmark("https://b.hatena.ne.jp/entrylist?url=docs.google.com/presentation&mode=rss");
+    private static final Logger logger = LoggerFactory.getLogger(GoogleSlideCollector.class);
+
+    private final Bookmark bookmark;
+
+    public GoogleSlideCollector() {
+        this.bookmark = new HatenaBookmark("https://b.hatena.ne.jp/entrylist?url=docs.google.com/presentation&mode=rss");
+    }
+
+    public GoogleSlideCollector(Bookmark bookmark) {
+        this.bookmark = bookmark;
+    }
 
     @Override
     public Stream<Slide> collect() throws JAXBException, MalformedURLException {
-        return collector.get()
-                .filter(i -> !i.title.equals("Google スライド - オンラインでプレゼンテーションを作成/編集できる無料サービスです"))
-                .peek(i -> i.link = i.link.replaceAll("\\?.*", ""))
-                .peek(i -> i.link = i.link.replaceAll("#.*", ""))
-                .peek(i -> i.link = i.link.replaceAll("/mobilepresent$", "/edit"))
-                .peek(i -> i.link = i.link.replaceAll("/preview$", "/edit"))
-                .peek(i -> i.title = i.title.replaceAll(" - Google スライド", ""))
-                .map(i -> new Slide(i, getAuthor(i.link)));
+        return bookmark.get()
+                .map(i -> getSlide(i).orElse(null))
+                .filter(Objects::nonNull);
+    }
+
+    private static Optional<Slide> getSlide(Item item) {
+        try {
+            String link = item.link;
+            Document doc = Jsoup.connect(link).get();
+
+            // 権限が必要なページの場合、docs.google.com から accounts.google.com にリダイレクトされる。
+            // その場合、プレゼンテーションは取得できないので処理をスキップする。
+            URL location = new URL(doc.location());
+            if (!location.getHost().equals("docs.google.com")) {
+                return Optional.empty();
+            }
+
+            // /pub の場合、embedURL は取得できない
+            if (!location.getPath().endsWith("/pub")) {
+                // URL を正規化
+                // (GoogleDocs の場合、embedURL から /preview のページを正とする)
+                Optional<String> embed = doc.getElementsByTag("meta")
+                        .stream()
+                        .filter(e -> e.attr("itemprop").equals("embedURL"))
+                        .findFirst()
+                        .map(e -> e.attr("content"));
+
+                // embedURL を取得できない場合、プレゼンテーションページではないと判断
+                if (embed.isEmpty()) {
+                    return Optional.empty();
+                }
+
+                if (!link.equals(embed.get())) {
+                    link = embed.get();
+                    doc = Jsoup.connect(link).get();
+                }
+            }
+
+            // アンカーやクエリーを取り除いたURLを生成
+            // (これらの有無によって、コンテンツは変わらないため)
+            URL url = new URL(link);
+            link = new URL(url.getProtocol(), url.getHost(), url.getPath()).toString();
+
+            String title = doc.getElementsByTag("meta")
+                    .stream()
+                    .filter(e -> e.attr("property").equals("og:title"))
+                    .findFirst()
+                    .map(e -> e.attr("content"))
+                    .orElse(doc.title().replaceAll(" - Google スライド", ""));
+            Optional<String> author = getAuthor(link);
+
+            return Optional.of(new Slide(title, link, item.date, author));
+        } catch (IOException e) {
+            logger.warn("Can't get SpeakerDeck document.", e);
+            return Optional.empty();
+        }
     }
 
     private static Optional<String> getAuthor(String link) {
---
+++ SlideCollector/src/test/java/tech/slideshare/collector/GoogleSlideCollectorTest.java
@@ -0,0 +1,98 @@
+package tech.slideshare.collector;
+
+import jakarta.xml.bind.JAXBException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import tech.slideshare.rss.Item;
+
+import java.net.MalformedURLException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GoogleSlideCollectorTest {
+
+    private Item getItem() {
+        var item = new Item();
+        item.title = "YAVIの紹介";
+        item.link = "https://docs.google.com/presentation/u/0/d/1aw5cXqTxvMbBvTUU4TiF2oTUkG8orDujA5IRQGtIDiw/preview";
+        item.description = "YAVIの紹介 Toshiaki Maki (@making), https://ik.am JSUG勉強会 2021-07-02 質問はTwitter(#jsug)にお願いします";
+        item.date = "2021-09-09T13:53:13Z";
+        item.subject = "テクノロジー";    // TODO: 複数あるみたい
+
+        return item;
+    }
+
+    @Test
+    public void collect() throws MalformedURLException, JAXBException {
+        var item = getItem();
+        var collector = new GoogleSlideCollector(() -> Stream.of(item));
+
+        List<Slide> slides = collector.collect().collect(Collectors.toList());
+        assertEquals(1, slides.size());
+
+        Slide slide = slides.get(0);
+        assertEquals(item.title, slide.getTitle());
+        assertEquals(item.link, slide.getLink());
+        assertEquals(Optional.empty(), slide.getAuthor());
+        assertEquals(ZonedDateTime.of(2021, 9, 9, 22, 53, 13, 0, ZoneId.of("Asia/Tokyo")), slide.getDate());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "https://docs.google.com/presentation/u/0/d/1aw5cXqTxvMbBvTUU4TiF2oTUkG8orDujA5IRQGtIDiw/edit",
+            "https://docs.google.com/presentation/u/0/d/1aw5cXqTxvMbBvTUU4TiF2oTUkG8orDujA5IRQGtIDiw/edit?usp=embed_googleplus",
+            "https://docs.google.com/presentation/u/0/d/1aw5cXqTxvMbBvTUU4TiF2oTUkG8orDujA5IRQGtIDiw/edit#slide=id.p",
+            "https://docs.google.com/presentation/u/0/d/1aw5cXqTxvMbBvTUU4TiF2oTUkG8orDujA5IRQGtIDiw/mobilepresent",
+    })
+    public void 正規化(String link) throws MalformedURLException, JAXBException {
+        var item = getItem();
+        item.link = link;
+        var collector = new GoogleSlideCollector(() -> Stream.of(item));
+
+        List<Slide> slides = collector.collect().collect(Collectors.toList());
+        assertEquals(1, slides.size());
+
+        Slide slide = slides.get(0);
+        assertEquals("https://docs.google.com/presentation/u/0/d/1aw5cXqTxvMbBvTUU4TiF2oTUkG8orDujA5IRQGtIDiw/preview", slide.getLink());
+    }
+
+    @Test
+    public void publicを正規化() throws MalformedURLException, JAXBException {
+        var item = getItem();
+        item.title = "The newsletter of RBS updates - RubyKaigi Takeout 2021 - Google スライド";
+        item.link = "https://docs.google.com/presentation/d/e/2PACX-1vREU6ZguqLxGk_k1l3zvKbRo_TbMTKN3yEgfzrjA85foVXrmeYvWnOTefsaBycsb9m6H924VsZw_YKt/pub?start=false&loop=false&delayms=3000&slide=id.p";
+        item.description = "The newsletter of RBS updates RubyKaigi Takeout 2021 Sep. 10th それでは、The newsletter of RBS updatesというタイトルで話させていただきたいと思います。 ----- 25 mins https://rubykaigi.org/2021-takeout Proposal";
+        item.date = "2021-09-12T06:24:05Z";
+        item.subject = "テクノロジー";
+        var collector = new GoogleSlideCollector(() -> Stream.of(item));
+
+        List<Slide> slides = collector.collect().collect(Collectors.toList());
+        assertEquals(1, slides.size());
+
+        Slide slide = slides.get(0);
+        assertEquals("https://docs.google.com/presentation/d/e/2PACX-1vREU6ZguqLxGk_k1l3zvKbRo_TbMTKN3yEgfzrjA85foVXrmeYvWnOTefsaBycsb9m6H924VsZw_YKt/pub", slide.getLink());
+    }
+
+    @Test
+    public void accessDenied() throws MalformedURLException, JAXBException {
+        var item = getItem();
+        item.title = "Google スライド - オンラインでプレゼンテーションを作成/編集できる無料サービスです";
+        item.link = "https://docs.google.com/presentation/d/1PjqrNO4r0-lRcJrNls-iT2CJ-kubNY31c9eJVo_eFSk/edit?resourcekey=0-pyJknY9TXGs9BUh0F0UjpA";
+        item.description = "パソコン、携帯電話、タブレットで新しいプレゼンテーションを作成し、他のユーザーと同時に共同編集できます。インターネット接続の有無に関係なく作業できます。Google スライドを使用して、PowerPoint ファイルを編集できます。本サービスは Google から無料で提供されています。";
+        item.date = "2021-09-09T13:53:13Z";
+        item.subject = "テクノロジー";
+
+        var collector = new GoogleSlideCollector(() -> Stream.of(item));
+
+        List<Slide> slides = collector.collect().collect(Collectors.toList());
+        assertEquals(0, slides.size());
+    }
+
+}
