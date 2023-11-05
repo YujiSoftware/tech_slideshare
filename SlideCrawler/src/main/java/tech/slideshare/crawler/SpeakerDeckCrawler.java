3dc7984f8fa8b160efa3d9a11121dc1eee2eef3c
YujiSoftware <yuji.software+github@gmail.com>
2023-11-06 00:52:51 +0900

    SpeakerDeck クローラーを、文字化けしないように修正

--- SlideCrawler/src/main/java/tech/slideshare/crawler/SpeakerDeckCrawler.java
+++ SlideCrawler/src/main/java/tech/slideshare/crawler/SpeakerDeckCrawler.java
@@ -1,15 +1,23 @@
 package tech.slideshare.crawler;
 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.io.RandomAccessReadBuffer;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SpeakerDeckCrawler implements Crawler {
 
@@ -22,28 +30,48 @@ public class SpeakerDeckCrawler implements Crawler {
         List<String> contents = new ArrayList<>();
 
         Document doc = Jsoup.connect(url).get();
-        Elements elements = doc.select("#transcript ol li div.slide-transcript p");
+        Elements elements = doc.select("a[title='Download PDF']");
         if (elements.isEmpty()) {
             return contents;
         }
 
-        StringBuilder builder = new StringBuilder();
-        for (Element element : elements) {
-            builder.setLength(0);
+        String link = elements.get(0).attr("href");
+        Path pdf = download(link);
 
-            List<Node> nodes = element.childNodes();
-            for (Node node : nodes) {
-                if (node.nodeName().equals("br")) {
-                    builder.append("\n");
-                } else if (node instanceof TextNode n) {
-                    builder.append(n.text());
-                } else if (node instanceof Element e) {
-                    builder.append(e.text());
+        Path fixed = Files.createTempFile(null, null);
+        Process process = new ProcessBuilder("pdf-fix-tuc", pdf.toString(), fixed.toString()).start();
+        try {
+            process.waitFor();
+        } catch (InterruptedException ignored) {
+        }
+
+        try (InputStream is = Files.newInputStream(fixed)) {
+            try (PDDocument document = Loader.loadPDF(new RandomAccessReadBuffer(is))) {
+                PDFTextStripper pdfStripper = new PDFTextStripper();
+                int page = document.getNumberOfPages();
+                for (int i = 1; i <= page; i++) {
+                    pdfStripper.setStartPage(i);
+                    pdfStripper.setEndPage(i);
+                    String text = pdfStripper.getText(document);
+
+                    String s =
+                            Arrays.stream(text.trim().split("\n"))
+                            .map(String::trim)
+                            .collect(Collectors.joining("\n"));
+                    contents.add(s);
                 }
             }
-            contents.add(builder.toString().trim());
         }
 
         return contents;
+    }
+
+    private static Path download(String link) throws IOException {
+        Path temp = Files.createTempFile(null, null);
+        try (InputStream stream = URI.create(link).toURL().openStream()) {
+            Files.copy(stream, temp, StandardCopyOption.REPLACE_EXISTING);
+        }
+
+        return temp;
     }
 }
--- SlideCrawler/src/test/java/tech/slideshare/crawler/SpeakerDeckCrawlerTest.java
+++ SlideCrawler/src/test/java/tech/slideshare/crawler/SpeakerDeckCrawlerTest.java
@@ -21,29 +21,34 @@ class SpeakerDeckCrawlerTest {
                 @catshun_
                 2023.08.07 Geography&Language 勉強会 #4
                 https://sites.google.com/view/geography-and-language/studygroup
-                • LLM 活⽤事例の⼀つとして プログラムベース推論の事例 を紹介
-                • 勉強会を通してプログラムベース推論の⻑所・短所を議論し、
-                LLM 活⽤の選択肢を増やす ことに議論を終着させる""", actual.get(0));
+                • LLM 活用事例の一つとして プログラムベース推論の事例 を紹介
+                • 勉強会を通してプログラムベース推論の長所・短所を議論し、
+                LLM 活用の選択肢を増やす ことに議論を終着させる""", actual.get(0));
         assertEquals("""
-                P.1
-                • 勉強会⽤に突貫的に作成しており、内容に誤りがある可能性が⼤いにあります...
-                • 内容の誤りや引⽤漏れ等がありましたらお伝えください
-                • 発⾔は confabulation を含みます
-                • 本勉強会は LLM 応⽤に関⼼のある⽅向けの発表を想定しています
-                • 著者は地理空間情報の初学者であり ”学びたい” というスタンスで発表いたします
-                留意事項""", actual.get(1));
+                P.80
+                • Ribeiro+’23 - Testing Language Models (and Prompts) Like We Test Software / TowardsDataScience Blog
+                https://towardsdatascience.com/testing-large-language-models-like-we-test-software-92745d28a359
+                • James Murdza+’23 - Evaluating code generation agents — LangChain and CodeChain
+                https://medium.com/@jamesmurdza/evaluating-llms-on-code-generation-langchain-and-codechain-5a804cb1e31c
+                LLM × テスト""", actual.get(79));
     }
 
     @Test
-    void fallback() throws IOException {
+    void mojibake() throws IOException {
         SpeakerDeckCrawler crawler = new SpeakerDeckCrawler();
         List<String> actual = crawler.crawl("https://speakerdeck.com/01rabbit/mr-dot-rabbit-wen-itashi-haarukedo-shi-ji-nihajian-tashi-ganaihatukingugazietuto");
 
+        assertEquals(31, actual.size());
         assertEquals("""
                 聞いた事はあるけど、実際には見た事がない
                 ハッキングガジェット
                 2017/12/2
                 元祖 濱せっく #1
-                @01ra66it""", actual.get(1));
+                @01ra66it""", actual.get(0));
+        assertEquals("""
+                厨二病の妄想と思った方は笑ってやってくださいw
+                もし、私と同じ様に脅威となりうると感じた方がいらっ
+                しゃいましたら一緒に対応策を考えてみませんか？
+                おわり""", actual.get(30));
     }
 }
