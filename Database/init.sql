d337a821b44625d49c218853322a3a18c3c726b3
YujiSoftware <yuji.software+github@gmail.com>
2023-11-27 00:14:44 +0900

    現在のデータベース定義を反映

--- Database/init.sql
+++ Database/init.sql
@@ -2,24 +2,30 @@ CREATE DATABASE tech_slideshare;
 USE tech_slideshare;
 
 CREATE TABLE slide (
-  slide_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title    TEXT NOT NULL,
-  url      VARCHAR(1024) NOT NULL UNIQUE,
-  author   TEXT,
-  twitter  VARCHAR(15),
-  crawled_flag BOOLEAN NOT NULL DEFAULT FALSE,
-  date     TIMESTAMP NOT NULL
+  slide_id integer NOT NULL AUTO_INCREMENT,
+  title text NOT NULL,
+  url varchar(768) NOT NULL,
+  author text DEFAULT NULL,
+  twitter varchar(15) DEFAULT NULL,
+  description text DEFAULT NULL,
+  image varchar(2048) DEFAULT NULL,
+  crawled_flag tinyint(1) NOT NULL DEFAULT 0,
+  date timestamp NOT NULL DEFAULT current_timestamp(),
+  updated_at timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (slide_id),
+  UNIQUE KEY url (url)
 );
 
 CREATE TABLE tweet_queue (
-  slide_id   INT NOT NULL PRIMARY KEY,
+  slide_id integer NOT NULL,
+  PRIMARY KEY (slide_id),
   FOREIGN KEY (slide_id) REFERENCES slide (slide_id)
 );
 
 CREATE TABLE content (
-  slide_id INT NOT NULL,
-  page     INT NOT NULL,
-  content  TEXT NOT NULL,
-  PRIMARY KEY(slide_id, page),
+  slide_id integer NOT NULL,
+  page integer NOT NULL,
+  content text NOT NULL,
+  PRIMARY KEY (slide_id,page),
   FOREIGN KEY (slide_id) REFERENCES slide (slide_id)
 );
