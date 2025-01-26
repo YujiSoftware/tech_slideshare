7e312dde0216ac3a067c920bf4b29bd15d931720
YujiSoftware <yuji.software+github@gmail.com>
2025-01-26 18:51:48 +0900

    不安定なテストを改善

--- SlideCollector/src/test/java/tech/slideshare/Assertions.java
+++ SlideCollector/src/test/java/tech/slideshare/Assertions.java
@@ -2,6 +2,8 @@ package tech.slideshare;
 
 import tech.slideshare.collector.Slide;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
@@ -11,6 +13,16 @@ public class Assertions {
         assertEquals(expected.getAuthor(), actual.getAuthor());
         assertEquals(expected.getTwitter(), actual.getTwitter());
         assertEquals(expected.getDescription(), actual.getDescription());
-        assertEquals(expected.getImage(), actual.getImage());
+
+        // maybe null
+        if (!Objects.equals(expected.getImage(), actual.getImage())) {
+            // パスの最後の部分は無視して比較（頻繁に変わるため）
+            // IN:  https://lh7-us.googleusercontent.com/docs/AHkbwyLwuNZGNYytiZ4VwzBu9g9SEp8r_Z46TrP2ztYKXJDM9zepXHT6zmYtoABbdSrAnKfoU9BvARHvby6TDKJb-XaF9bzyRUeVDLDgvPSfbj4FxXRAW0Z-=w1200-h630-p
+            // OUT: https://lh7-us.googleusercontent.com/docs/
+            assertEquals(
+                    expected.getImage().replaceFirst("(.*/)[^/]+", "$1")
+                    , actual.getImage().replaceFirst("(.*/)[^/]+", "$1")
+            );
+        }
     }
 }
